34a6db5b117e97856288b024fd0d6ac9400861e8
root <132725436+tingtung46@users.noreply.github.com>
2025-04-30 23:46:59 +0700

    Add index with ejs

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("node:path");
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { message: "EJS rocks!" });
+});
+
+app.listen(3000);
