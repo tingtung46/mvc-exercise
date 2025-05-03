3c07ac3eaeeb7b425ab7e0763aae14502a099a56
root <132725436+tingtung46@users.noreply.github.com>
2025-05-03 11:55:07 +0700

    Use ejs to generate page content

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express = require("express");
 const path = require("node:path");
 const app = express();
 
+const assetsPath = path.join(__dirname, "public");
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(assetsPath));
 app.get("/", (req, res) => {
-  res.render("index", { message: "EJS rocks!" });
+  res.render("index", { links: links, users: users });
 });
 
 app.listen(3000);
